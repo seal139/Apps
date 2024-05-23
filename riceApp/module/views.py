b6b22166cf94f9d77c668da05af3ffad894d5a89
andikaap13 <andikaap13@gmail.com>
2024-05-23 10:32:22 +0700

    Add AdminLTE

--- riceApp/module/views.py
+++ riceApp/module/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
-  template = loader.get_template('home.html')
+  template = loader.get_template('index.html')
   return HttpResponse(template.render())
